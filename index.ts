85227e1ccf7490699274061796e570aa97b5ea72
TAYYAB ILYAS <tht9935@gmail.com>
2024-03-23 15:21:21 +0500

    created a number guessing game

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+ 
+
+const randomNumber = Math.floor(Math.random()*5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name:"userinput",
+        message:"please guess a number Between 1-5",
+        type:"number",
+        
+        
+    }
+]);
+
+if(randomNumber === answer.userinput){
+    console.log("you Win --- U guess a Same NUmber ");
+    console.log(`Answer is ${randomNumber}`);
+    
+    
+}
+else{
+    console.log("you Lose --- U guess A Different Number");
+    console.log(`Answer is ${randomNumber}`);
+
+}
